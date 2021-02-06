26aec1e554e898fe6bed593246e9952744465104
Fiona Gunawan <fiona.gunawan5@yahoo.com>
2021-02-06 14:02:22 -0600

    AboutUs section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"; //don't need to import react anymore in react 17 version
+//import pgs
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
