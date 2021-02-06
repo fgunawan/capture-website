b8d611e9238373bc9ed4db6f37e123ab1a357f87
Fiona Gunawan <fiona.gunawan5@yahoo.com>
2021-02-06 15:15:10 -0600

    AboutSection styling with styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"; //don't need to import react anymore in react 17 version
+import GlobalStyle from "./components/GlobalStyle";
 //import pgs
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,64 @@
 import React from "react";
 import home1 from '../img/home1.png';
+//import styled pkg - we are using the styled components method of styling here which is very common
+//download vscode-styled-components extension along with pkg for txt highlighting
+import styled from 'styled-components';
 
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+        <StyledAbout>
+            <StyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>your <span>dreams</span>
                         </h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>come true.</h2>
-                    </div>
+                    </StyledHide>
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. 
                     We have professionals with amazing skills.
                 </p>
                 <button>Contact Us</button>
-            </div>
-            <div className="img">
+            </StyledDescription>
+            <StyledImage>
                 <img src={home1} alt="guy with a camera"/>
-            </div>
-        </div>
+            </StyledImage>
+        </StyledAbout>
     )
 }
+
+//styled component - to add styling to pg, replace the tag tht u want to style with the const name of the style
+const StyledAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+const StyledDescription = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2{
+        font-weight: lighter;
+    }
+`;
+const StyledImage = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
+const StyledHide = styled.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
