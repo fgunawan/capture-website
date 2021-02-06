4b3894579c71c67437298c7cfcbc8a17e311e223
Fiona Gunawan <fiona.gunawan5@yahoo.com>
2021-02-06 17:12:07 -0600

    Creating and styling Nav component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react"; //don't need to import react anymore in react 17 vers
 import GlobalStyle from "./components/GlobalStyle";
 //import pgs
 import AboutUs from './pages/AboutUs';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
